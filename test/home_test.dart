014d5325c1005888f54d4b921acdb41aa0795f63
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-09-30 16:52:25 -0300

    Add home_test.dart

---
+++ test/home_test.dart
@@ -0,0 +1,37 @@
+import 'package:estilizacao_componentes/components/box_card.dart';
+import 'package:estilizacao_componentes/data/bank_inherited.dart';
+import 'package:estilizacao_componentes/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('My widget has a text "Spent" ', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: BankInherited(child: Home())));
+    final spentFinder = find.text('Spent');
+    expect(spentFinder, findsOneWidget);
+  });
+
+  testWidgets('finds a LinearProgressIndicator', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: BankInherited(child: Home())));
+    expect(find.byType(LinearProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('finds account status widget', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: BankInherited(child: Home())));
+    expect(find.byKey(const Key('testKey')), findsOneWidget);
+  });
+
+  testWidgets('finds 5 boxcards', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: BankInherited(child: Home())));
+    expect(
+      find.byWidgetPredicate((widget) {
+        if (widget is BoxCard) {
+          return true;
+        } else {
+          return false;
+        }
+      }),
+      findsNWidgets(5),
+    );
+  });
+}
